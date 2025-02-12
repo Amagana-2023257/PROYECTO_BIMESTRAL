9bc9f77e37877e32224807745973481022887c2d
Informatica <informatica@alumno.local>
2025-02-12 13:54:20 -0600

    build: basic config

---
+++ configs/swagger.js
@@ -0,0 +1,36 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title: "Ventas System API",
+            version:"1.0.0",
+            description: "API para sistema de ventas",
+            contact:{
+                name: "Angel Magaña",
+                email: "amagana-2023257@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/ventas/v1"
+            }
+        ]
+    },
+    apis:[
+        "./src/auth/*.js",
+        "./src/cart/*.js",
+        "./src/category/*.js",
+        "./src/invoice/*.js",
+        "./src/payment/*.js",
+        "./src/product/*.js",
+        "./src/user/*.js",
+
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+
+export { swaggerDocs, swaggerUi }
